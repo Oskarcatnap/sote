673d2983196c9523a905bd0999e329ad3b973b29
Oskarcatnap <ggoskarcatnap@gmail.com>
2026-01-22 19:44:57 +0200

    Refactor app.py for clarity and updates

--- app.py
+++ app.py
@@ -1,22 +1,20 @@
+import os
 from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
 
-# Твоя ссылка на Google Drive
+# Ссылка на твой файл в Google Drive
 DRIVE_LINK = "https://drive.google.com/uc?export=download&id=17P6kKliwJHqVWqNlT4o-NlwMeZn-F-Mr"
 
 @app.route('/')
-def index():
-    # Показываем главную страницу
+def home():
+    # ВОТ ОНА! Эта строчка говорит: "Возьми дизайн из templates/index.html"
     return render_template('index.html')
 
 @app.route('/download')
-def download_file():
-    # Когда нажимают кнопку, Python сам перекидывает на файл
-    print("Кто-то нажал кнопку скачать!")
+def download():
     return redirect(DRIVE_LINK)
 
-if __name__ == '__main__':
-    import os
+if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
